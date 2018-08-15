02bcd65acc3999381ae9159a0ed29d855ed70c49
Daniel Valsby-Koch <dava@aspit.dk>
2018-08-15 22:39:33 +0200

    Add project files.

---
+++ GithubTesting/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GithubTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is a simple project not version controlled from the beginning");
+            Console.WriteLine("But it will be ...");
+        }
+    }
+}
